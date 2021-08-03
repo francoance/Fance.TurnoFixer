5f7702903f8720fd781f992f6e6b965622650cc0
Franco Ance <franco.ance@gmail.com>
2021-08-03 00:07:23 -0300

    Initial commit

---
+++ Fance.TurnoFixer.Models/ChatStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Fance.TurnoFixer.Models
+{
+    public class ChatStatus
+    {
+        public long ChatId { get; set; }
+        
+        public ChatStatus(long chatId)
+        {
+            ChatId = chatId;
+        }
+    }
+}
---
+++ Fance.TurnoFixer/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fance.TurnoFixer.TelegramBot;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Fance.TurnoFixer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) => { services.AddHostedService<TurnoFixerBot>(); });
+    }
+}
